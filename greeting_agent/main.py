d391a8bb30acac6ef0c0e6cc550f1f783bd3b2b7
antara-1505 <antarashaw515@gmail.com>
2025-11-30 13:16:17 +0530

    updated requirements

--- greeting_agent/main.py
+++ greeting_agent/main.py
@@ -46,4 +46,4 @@ def health_check():
 if __name__ == "__main__":
     # run_all_agents()
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
